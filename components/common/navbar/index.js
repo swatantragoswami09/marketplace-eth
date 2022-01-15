375abfb1a052a128afa2fb2fb4cb6ffd1ba1c9c2
swatantra.goswami <swatantra.goswami_cs17@gla.ac.in>
2022-01-15 21:40:48 +0530

    updated code

---
+++ components/common/navbar/index.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <section>
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav className="relative" aria-label="Global">
+          <div className="flex justify-between">
+            <div>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Home
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  MarketPlace
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Blog
+                </a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Wishlist
+                </a>
+              </Link>
+
+              <a
+                href="#"
+                className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Connect
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+}
