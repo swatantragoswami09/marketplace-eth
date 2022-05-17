baf2bee02f7c5da280e8e432eca4dea3fb3aa67a
swatantragoswami09 <swatantra.goswami_cs17@gla.ac.in>
2022-05-17 22:18:33 +0530

    new branch MP-feature1

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,4 +1,5 @@
 import { useWalletInfo } from "@components/hooks/web3";
+import { useWeb3 } from "@components/providers";
 import { Breadcrumbs, Button } from "@components/ui/common";
 
 import { CourseCard, CourseList } from "@components/ui/course";
@@ -11,11 +12,41 @@ import { getAllCourses } from "@content/courses/fetcher";
 import { useState } from "react";
 
 export default function Marketplace({ courses }) {
-  const [selectedCourse, setSelectedCourse] = useState(null);
-  const { canPurchaseCourse } = useWalletInfo();
+  const { web3, contract } = useWeb3();
 
-  const purchaseCourse = (order) => {
-    alert(JSON.stringify(order));
+  const { canPurchaseCourse, account } = useWalletInfo();
+  const [selectedCourse, setSelectedCourse] = useState(null);
+
+  const purchaseCourse = async (order) => {
+    console.log("contract-", contract);
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
+    console.log("courseId-", hexCourseId);
+
+    const orderHash = web3.utils.soliditySha3(
+      { type: "bytes16", value: hexCourseId },
+      { type: "address", value: account.data }
+    );
+    console.log("orderHash-", orderHash);
+
+    const emailHash = web3.utils.sha3(order.email);
+    console.log("emailHash-", emailHash);
+
+    const proof = web3.utils.soliditySha3(
+      { type: "bytes32", value: emailHash },
+      { type: "bytes32", value: orderHash }
+    );
+    console.log("prrof-", proof);
+
+    const value = web3.utils.toWei(String(order.price));
+    console.log("value-", value);
+    try {
+      const result = await contract.methods
+        .purchaseCourse(hexCourseId, proof)
+        .send({ from: account.data, value });
+      console.log(result);
+    } catch {
+      console.error("Purchase course: Operation has failed.");
+    }
   };
 
   return (
